ad4747b26e5aeddb0674f931f9b247d9b2d8f471
Jan Harvey Jimenez <janharvs98@gmail.com>
2022-09-28 09:13:55 +0800

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Employee from './components/employee';
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? 
         <>
+          <input 
+            type='text' 
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
           <Employee name ="Caleb" role="Intern"/>
-          <Employee name ="Abby"/>
+          <Employee name ="Abby" role={role}/>
           <Employee name ="Jeni"/>
           <Employee name ="John"/>
           <Employee name ="Art"/>
